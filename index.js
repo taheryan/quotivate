8d79745c7c1e22780be150aabbcf75b9ba6aad01
Erfan Taheryan <erfantaheryan@gmail.com>
2024-04-15 18:38:35 +0330

    update index.js and package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Importing the Command class from Commander.js library
 import { Command } from "commander";
-import { getQuote } from "./commands/get-quote";
+import { getQuote } from "./commands/get-quote.js";
 
 // Creating an instance of the Command class
 const program = new Command();
@@ -15,8 +15,7 @@ program
   )
   .version("1.0.0");
 
-// Defining a command called 'add'
-program.command("").description("Create a new todo.").action(getQuote);
+program.action(getQuote);
 
 // Parsing the command-line arguments and executing the corresponding actions
 program.parse();
